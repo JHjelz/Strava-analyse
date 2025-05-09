b51aad0365f8c2207e48f2382c0d91390423f787
jshjelse <jshjelse@stud.ntnu.no>
2025-05-09 21:08:45 +0200

    Fix index

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 # app.py
 
 from flask import Flask, render_template
+from flask_cors import CORS
 
 from backend.access import access_bp, socketio
 from backend.activities import activities_bp
 
 app = Flask(__name__)
+CORS(app, origins=["https://jhjelz.github.io"])
 
 # Initialiser SocketIO med Flask-appen
 socketio.init_app(app)
